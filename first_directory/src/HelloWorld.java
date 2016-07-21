8319a77b650b4320df2ae0c5675acc2800eb3897
russell.lego <russell.lego@tubemogul.com>
2016-07-21 10:19:29 -0700

    first commit.

---
+++ first_directory/src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by russell.lego on 7/20/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!!");
+    }
+}
