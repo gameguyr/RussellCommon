8ec3675b60afe5e7a86e7824d02bd7f61d9ac740
russell.lego <russell.lego@tubemogul.com>
2018-02-01 10:58:03 -0800

    creating the Dog class

---
+++ learning/DogTestDrive.java
@@ -0,0 +1,17 @@
+public class DogTestDrive {
+    public static void main (String[] args) {
+        Dog one = new Dog();
+        one.size = 70;
+
+        Dog two = new Dog();
+        two.size = 25;
+
+        Dog three = new Dog();
+        three.size = 10;
+
+        one.bark();
+        two.bark();
+        three.bark();
+
+    }
+}
