b7c4e193643acd9a86947b4a309c709d666c43f7
russell.lego <russell.lego@tubemogul.com>
2021-01-12 16:16:22 -0800

    Adding a utility class for creating functions I use all the time

---
+++ src/main/java/RussellCommon.java
@@ -0,0 +1,40 @@
+public class RussellCommon {
+
+
+    public static String secondsToMinutesAndSeconds(int numSeconds) {
+        String minutesAndSeconds = "NUMBER_OF_MINUTES minutes NUMBER_OF_SECONDS seconds";
+        int numberOfSecondsPerMinute = 60;
+        int numberOfMinutes = numSeconds / numberOfSecondsPerMinute;
+        int numberOfSeconds = numSeconds % numberOfSecondsPerMinute;
+        minutesAndSeconds = minutesAndSeconds.replace("NUMBER_OF_MINUTES", Integer.toString(numberOfMinutes));
+        minutesAndSeconds = minutesAndSeconds.replace("NUMBER_OF_SECONDS", Integer.toString(numberOfSeconds));
+        return minutesAndSeconds;
+    }
+
+    public int minutesAndSecondsToSeconds(int numMinutes, int numSeconds) {
+        return (numMinutes * 60) + numSeconds;
+    }
+
+    /**
+     * This method takes in an array of integers for both minutes and seconds and returns the total number of seconds
+     * @param numMinutes an array of minutes
+     * @param numSeconds an array of corresponding seconds
+     * @return the total number of seconds
+     * @throws Exception just an exception when the two arrays are not the same size
+     */
+    public int minutesAndSecondsToSeconds(int[] numMinutes, int[] numSeconds) throws Exception {
+        if (numMinutes.length != numSeconds.length) {
+            throw new Exception("Arrays are not the same size");
+        }
+        int totalNumberSeconds = 0;
+        for (int i=0; i < numMinutes.length; i++) {
+            totalNumberSeconds += minutesAndSecondsToSeconds(numMinutes[i], numSeconds[i]);
+        }
+        return totalNumberSeconds;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(RussellCommon.secondsToMinutesAndSeconds(636));
+    }
+}
