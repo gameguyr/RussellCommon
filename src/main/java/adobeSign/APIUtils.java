4a12835f406f7714e85ee60d963a9e708e5d3261
russell.lego <russell.lego@tubemogul.com>
2021-06-02 09:21:30 -0700

    adding things I have been learning

---
+++ src/main/java/adobeSign/APIUtils.java
@@ -0,0 +1,18 @@
+package adobeSign;
+
+import com.mashape.unirest.http.Unirest;
+
+import java.net.http.HttpResponse;
+
+public class APIUtils {
+
+//    public HttpResponse<String> getAllWebhooks() throws Exception {
+//        Unirest.setTimeouts(0, 0);
+//        HttpResponse<String> response = Unirest.get("https://api.na1.echosignawspreview.com:443/api/rest/v6/webhooks?Authorization=Bearer%203AAABLKmtbUC8NMf6SXhvdY2AYadyl0lxW-mCe3quamzluAASYIdz9Bm-nr1oSr9s-oclRvYHsvnXNcFydTSRAOehbn30_Uzu")
+//                .header("Authorization", "Bearer 3AAABLKmtbUCcPXH4St9IgZY2wXJnp_wU3dpP15jKLX_v5ispDXb-COPWRiWZ14ymtlA3qp-MYFw*")
+//                .asString();
+//        return response;
+//    }
+
+
+}
